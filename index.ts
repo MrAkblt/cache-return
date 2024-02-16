3aee0c13fdf46c975f6836f3713c7281fbc642b2
murat akbulut <m.akblt@live.com>
2024-02-16 22:33:56 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,24 +1,18 @@
 export const cacheReturn = (cacheTTL: number) => (target: any, name: string, descriptor: any) => {
   const fn = descriptor.value;
-	const cache: { [key:string]: { returnValue: any, cacheDate: number } } = {};
+  const cache: { [key:string]: { returnValue: any, cacheDate: number } } = {};
   descriptor.value = function (...args: any[]) {
-		const key = JSON.stringify(args)
-    console.log(cache)
-    if(cache[key]) {
-      console.log(cache[key].cacheDate, cacheTTL, Date.now());
+    const key = JSON.stringify(args)
+    if (cache[key] && cache[key].cacheDate + cacheTTL > Date.now()) {
+      return cache[key].returnValue;
+    } else {
+      const returnValue = fn.apply(this, args);
+      cache[key] = {
+	returnValue,
+	cacheDate: Date.now(),
+      }
+      return returnValue;
     }
-		if (cache[key] && cache[key].cacheDate + cacheTTL > Date.now()) {
-      console.log("cacheden: ", cache[key].returnValue)
-			return cache[key].returnValue;
-		} else {
-			const returnValue = fn.apply(this, args);
-			cache[key] = {
-				returnValue,
-				cacheDate: Date.now(),
-			}
-      console.log("real: ", returnValue)
-			return returnValue;
-		}
   };
   return descriptor;
 }
