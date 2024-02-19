f4dc39b7772351358571a66d4e1bd63c0f18f57e
murat akbulut <m.akblt@live.com>
2024-02-19 09:29:45 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-export const cacheReturn = (cacheTTL: number) => (target: any, name: string, descriptor: any) => {
+export const cacheReturn = ({ cacheTTL }: { cacheTTL: number}) => (target: any, name: string, descriptor: any) => {
   const fn = descriptor.value;
   const cache: { [key:string]: { returnValue: any, cacheDate: number } } = {};
+  // This interval is only for the memory leak, not related logic
+	setInterval(() => {
+		for (const key in cache) {
+			if(cache[key].cacheDate + cacheTTL < Date.now()) {
+				delete cache[key];
+			}
+		}
+	}, 300000)
   descriptor.value = function (...args: any[]) {
     const key = JSON.stringify(args)
     if (cache[key] && cache[key].cacheDate + cacheTTL > Date.now()) {
@@ -8,8 +16,8 @@ export const cacheReturn = (cacheTTL: number) => (target: any, name: string, des
     } else {
       const returnValue = fn.apply(this, args);
       cache[key] = {
-        returnValue,
-        cacheDate: Date.now(),
+	      returnValue,
+	      cacheDate: Date.now(),
       }
       return returnValue;
     }
